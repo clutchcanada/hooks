7256852a1da986ae660738fdc64fcb6b79f871b9
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-06-10 17:24:27 -0400

    add useFormState

---
+++ src/useFormState/utils/index.js
@@ -0,0 +1,16 @@
+export const isValidTextEntry = R.anyPass([R.isNil, R.equals('')]);
+export const reduceFormKeysToState = R.pipe(
+  R.values,
+  R.reduce(
+    (accum, value) => ({
+      ...accum,
+      [value]: {
+        value: '',
+        error: false,
+        errorMessage: '',
+        disabled: false,
+      },
+    }),
+    {},
+  ),
+);
