ff6ee74557583eb5b3b4aa75e7a541f5b80a38af
Ahmed Panju <ahmed.a.panju@gmail.com>
2019-07-23 09:20:11 -0400

    fixed up formatting

--- src/useStepState/index.js
+++ src/useStepState/index.js
@@ -1,38 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { throwError } from '@clutch/helpers';
 
 export const useStepState = ({
-    steps = [],
-    currentStepIndex = 0,
-    useStateDep = useState,
-    useEffectDep = useEffect
+  steps = [],
+  currentStepIndex = 0,
+  useStateDep = useState,
+  useEffectDep = useEffect,
 }) => {
-    const [currentStepIndexState, setCurrentStepIndexState] = useStateDep(currentStepIndex);
-    const [currentStepState, setCurrentStepState] = useStateDep(steps[currentStepIndex]);
+  const [currentStepIndexState, setCurrentStepIndexState] = useStateDep(currentStepIndex);
+  const [currentStepState, setCurrentStepState] = useStateDep(steps[currentStepIndex]);
 
-    useEffectDep(() => {
-        !!steps[currentStepIndexState] ? 
-        setCurrentStepState(steps[currentStepIndexState]) :
-        throwError('This step does not exist');
-    }, [currentStepIndexState]);
+  useEffectDep(() => {
+    steps[currentStepIndexState]
+      ? setCurrentStepState(steps[currentStepIndexState])
+      : throwError('You are trying to go to a step that does not exist');
+  }, [currentStepIndexState]);
 
-    const nextStep = () => {
-        setCurrentStepIndexState(currentStepIndexState + 1);
-    }
+  const nextStep = () => {
+    setCurrentStepIndexState(currentStepIndexState + 1);
+  };
 
-    const previousStep = () => {
-        setCurrentStepIndexState(currentStepIndexState - 1);
-    }
+  const previousStep = () => {
+    setCurrentStepIndexState(currentStepIndexState - 1);
+  };
 
-    const resetSteps = () => {
-        setCurrentStepIndexState(0);
-        setCurrentStepState(steps[0]);
-    }
+  const resetSteps = () => {
+    setCurrentStepIndexState(0);
+  };
 
-    return {
-        currentStep: currentStepState,
-        nextStep
-    }
-}
+  return {
+    currentStep: currentStepState,
+    nextStep,
+    previousStep,
+    resetSteps,
+  };
+};
 
 export default useStepState;
