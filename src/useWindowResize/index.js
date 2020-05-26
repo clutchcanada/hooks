57b2983f9a3de0662fbdb61c4135c03c8bcaf36b
Craig Pullar <craigmichaelpullar@gmail.com>
2020-05-26 11:27:54 -0400

    fix window resize

--- src/useWindowResize/index.js
+++ src/useWindowResize/index.js
@@ -3,23 +3,12 @@ import { useEffect, useState } from 'react';
 const useWindowResize = ({
   useStateDep = useState,
   useEffectDep = useEffect,
-  debounce = 0,
 } = {}) => {
   const [windowWidth, setWindowWidth] = useStateDep(window.innerWidth);
 
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  },[value]);
-
-  const updateWindowWidth = setTimeout(() => {
-      setWindowWidth(value);
-    }, delay);
+  const updateWindowWidth = () => {	
+    setWindowWidth(window.innerWidth);
+  };
   useEffectDep(() => {
     window.addEventListener('resize', updateWindowWidth);
     return () => window.removeEventListener('resize', updateWindowWidth);
