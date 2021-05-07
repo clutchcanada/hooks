8bc50dc1b50a8cc803d994ad70b403d5dfa3e56f
Jordan Cain <jordan.cain@clutch.ca>
2021-05-07 16:38:31 -0400

    feat(windowResize): Add debounc eby default to windowResize hook

--- src/useWindowResize/index.js
+++ src/useWindowResize/index.js
@@ -3,15 +3,28 @@ import { useEffect, useState } from 'react';
 const useWindowResize = ({
   useStateDep = useState,
   useEffectDep = useEffect,
+  debounce = true,
 } = {}) => {
   const [windowWidth, setWindowWidth] = useStateDep(window.innerWidth);
+  const [timeoutState, setTimeoutState] = useStateDep();
 
   const updateWindowWidth = () => {
     setWindowWidth(window.innerWidth);
   };
+
+  const debouncedUpdateWindowWidth = () => {
+    const timeout = setTimeout(() => {
+      updateWindowWidth();
+    }, 400);
+    setTimeoutState(timeout);
+  }
+
   useEffectDep(() => {
-    window.addEventListener('resize', updateWindowWidth);
-    return () => window.removeEventListener('resize', updateWindowWidth);
+    window.addEventListener('resize', debounce ? debouncedUpdateWindowWidth : updateWindowWidth);
+    return () => {
+      window.removeEventListener('resize', debounce ? debouncedUpdateWindowWidth : updateWindowWidth);
+      clearTimeout(timeoutState);
+    }
   }, []);
 
   return {
--- src/useWindowResize/index.test.js
+++ src/useWindowResize/index.test.js
@@ -1,11 +1,30 @@
 import useWindowResize from "./index";
 
+jest.useFakeTimers();
+
 describe("useWindowResize", () => {
   it('should call setState on window resize', () => {
     const setStateMock = jest.fn();
     useWindowResize({
       useStateDep: global.useStateMock({ setStateMock }),
       useEffectDep: (fn) => fn(),
+    });
+    global.innerWidth = 500;
+    global.dispatchEvent(new Event('resize'));
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    
+    jest.runAllTimers();
+
+    expect(setStateMock).toBeCalledWith(500);
+  });
+
+  it('should call setState on window resize without debounce', () => {
+    const setStateMock = jest.fn();
+    useWindowResize({
+      useStateDep: global.useStateMock({ setStateMock }),
+      useEffectDep: (fn) => fn(),
+      debounce: false
     });
     global.innerWidth = 500;
     global.dispatchEvent(new Event('resize'));
